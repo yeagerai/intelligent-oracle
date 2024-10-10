d98fef2fe1cae66ec13816d0a4a69d8a7646c24a
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-10-10 14:12:19 -0300

    contract: improve readability
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- intelligent-oracle.py
+++ intelligent-oracle.py
@@ -5,15 +5,65 @@ Temporary solutions:
 """
 
 from typing import Any
-import uuid
 from backend.node.genvm.icontract import IContract
 from backend.node.genvm.equivalence_principle import EquivalencePrinciple
+from enum import Enum
+
+
+class Status(Enum):
+    ACTIVE = "Active"
+    RESOLVED = "Resolved"
+    ERROR = "Error"
+
+
+class IntelligentOracle:
+
+    from datetime import datetime
+
+    def __init__(
+        self,
+        id: str,
+        creator: str,
+        title: str,
+        description: str,
+        potential_outcomes: list[str],
+        rules: list[str],
+        data_sources: list[str],
+        earliest_resolution_date: datetime,
+    ):
+        if not isinstance(id, str):
+            raise ValueError("ID must be a string.")
+        if id == "":  # TODO: should we have a predefined schema like uuid?
+            raise ValueError("ID cannot be empty.")
+
+        self.id = id
+        self.creator = creator
+        self.title = title
+        self.description = description
+        self.potential_outcomes = potential_outcomes
+        self.rules = rules
+        self.data_sources = data_sources
+        self.status = Status.ACTIVE
+        self.earliest_resolution_date = earliest_resolution_date
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "creator": self.creator,
+            "title": self.title,
+            "description": self.description,
+            "potential_outcomes": self.potential_outcomes,
+            "rules": self.rules,
+            "data_sources": self.data_sources,
+            "status": self.status.value,
+            "earliest_resolution_date": self.earliest_resolution_date.isoformat(),
+        }
 
 
 class IntelligentOracleFactory(IContract):
 
     def __init__(self):
-        self.oracles: dict[str, IntelligentOracleFactory.IntelligentOracle] = {}
+        self.oracles: dict[str, IntelligentOracle] = {}
 
     async def register(
         self,
@@ -27,7 +77,7 @@ class IntelligentOracleFactory(IContract):
         if id in self.oracles:
             raise ValueError(f"Oracle with ID {id} already exists.")
 
-        oracle = IntelligentOracleFactory.IntelligentOracle(  # TODO: Replace with actual values.
+        oracle = IntelligentOracle(  # TODO: Replace with actual values.
             id=id,
             creator="",
             title="",
@@ -44,53 +94,3 @@ class IntelligentOracleFactory(IContract):
         Get an oracle by its ID.
         """
         return self.oracles[oracle_id].to_dict()
-
-    class IntelligentOracle:
-
-        from datetime import datetime
-
-        def __init__(
-            self,
-            id: str,
-            creator: str,
-            title: str,
-            description: str,
-            potential_outcomes: list[str],
-            rules: list[str],
-            data_sources: list[str],
-            earliest_resolution_date: datetime,
-        ):
-            if not isinstance(id, str):
-                raise ValueError("ID must be a string.")
-            if id == "":  # TODO: should we have a predefined schema like uuid?
-                raise ValueError("ID cannot be empty.")
-
-            self.id = id
-            self.creator = creator
-            self.title = title
-            self.description = description
-            self.potential_outcomes = potential_outcomes
-            self.rules = rules
-            self.data_sources = data_sources
-            self.status = IntelligentOracleFactory.IntelligentOracle.Status.ACTIVE
-            self.earliest_resolution_date = earliest_resolution_date
-
-        def to_dict(self):
-            return {
-                "id": self.id,
-                "creator": self.creator,
-                "title": self.title,
-                "description": self.description,
-                "potential_outcomes": self.potential_outcomes,
-                "rules": self.rules,
-                "data_sources": self.data_sources,
-                "status": self.status.value,
-                "earliest_resolution_date": self.earliest_resolution_date.isoformat(),
-            }
-
-        from enum import Enum
-
-        class Status(Enum):
-            ACTIVE = "Active"
-            RESOLVED = "Resolved"
-            ERROR = "Error"
