a13de27c22a339258cadf2fbff863ffd138413f7
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-10-10 14:53:52 -0300

    contract: add features
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- intelligent-oracle.py
+++ intelligent-oracle.py
@@ -9,6 +9,20 @@ from backend.node.genvm.equivalence_principle import EquivalencePrinciple
 from enum import Enum
 from dataclasses import dataclass
 from datetime import datetime
+import re
+
+
+@dataclass
+class CheckTypes:
+    def __post_init__(self):
+        for name, field_type in self.__annotations__.items():
+            if not isinstance(self.__dict__[name], field_type):
+                current_type = type(self.__dict__[name])
+                raise TypeError(
+                    f"The field `{name}` was assigned by `{current_type}` instead of `{field_type}`"
+                )
+
+        print("Check is passed successfully")
 
 
 class Status(Enum):
@@ -18,18 +32,28 @@ class Status(Enum):
 
 
 @dataclass
-class IntelligentOracle:
+class IntelligentOracle(CheckTypes):
     global Status  # needed due to limitation in the simulator imports
     global datetime  # needed due to limitation in the simulator imports
 
     id: str
+    prediction_market_id: (
+        str  # Used to communicate back to the prediction market through the bridge
+    )
     creator: str
     title: str
     description: str
     potential_outcomes: list[str]
     rules: list[str]
-    data_sources: list[str]
-    earliest_resolution_date: datetime
+    valid_data_sources: list[
+        str
+    ]  # List of regex URL patterns for valid data sources. Users will provide data that matches on these patterns.
+    data_sources: list[
+        str
+    ]  # List of URLs that need to match with one of valid_data_sources
+    earliest_resolution_date: (
+        datetime  # Minimum date and time when the oracle can be resolved
+    )
     status: Status
 
     def __post_init__(self):
@@ -37,6 +61,41 @@ class IntelligentOracle:
             raise ValueError("ID must be a string.")
         if self.id == "":  # TODO: should we have a predefined schema like uuid?
             raise ValueError("ID cannot be empty.")
+
+        if len(self.potential_outcomes) < 2:
+            raise ValueError("At least two potential outcomes are required.")
+
+        for valid_data_source in self.valid_data_sources:
+
+            try:
+                re.compile(valid_data_source)
+            except re.error:
+                raise ValueError(
+                    f"Invalid regex pattern for data source: {valid_data_source}"
+                )
+
+        for data_source in self.data_sources:
+            if not any(
+                re.match(valid_data_source, data_source)
+                for valid_data_source in self.valid_data_sources
+            ):
+                raise ValueError(
+                    f"Data source {data_source} does not match any valid data source pattern."
+                )
+
+    def submit_data_source(self, data_source: str):
+
+        self.data_sources.append(data_source)
+
+    def resolve(self):
+        if datetime.now() < self.earliest_resolution_date:
+            raise ValueError("Cannot resolve before the earliest resolution date.")
+
+        if not self.data_sources:
+            raise ValueError("No data sources loaded.")
+
+        # TODO: call llms
+        self.status = Status.RESOLVED
 
     def to_dict(self):
         return {
@@ -46,6 +105,7 @@ class IntelligentOracle:
             "description": self.description,
             "potential_outcomes": self.potential_outcomes,
             "rules": self.rules,
+            "valid_data_sources": self.valid_data_sources,
             "data_sources": self.data_sources,
             "status": self.status.value,
             "earliest_resolution_date": self.earliest_resolution_date.isoformat(),
@@ -57,9 +117,9 @@ class IntelligentOracleFactory(IContract):
     def __init__(self):
         self.oracles: dict[str, IntelligentOracle] = {}
 
-    async def register(
+    def register(
         self,
-        id: str,
+        id: str,  # will change to oracleContractAddress in the future
     ) -> None:
         """
         Create a new oracle and register it with the factory.
@@ -71,16 +131,23 @@ class IntelligentOracleFactory(IContract):
 
         oracle = IntelligentOracle(  # TODO: Replace with actual values.
             id=id,
-            creator="",
+            creator=contract_runner.from_address,
             title="",
             description="",
             potential_outcomes=[],
             rules=[],
+            valid_data_sources=[],
             data_sources=[],
             earliest_resolution_date=datetime.now(),
             status=Status.ACTIVE,
         )
         self.oracles[id] = oracle
+
+    async def resolve(self, oracle_id: str):
+        await self.oracles[oracle_id].resolve()
+
+    def submit_data_source(self, oracle_id: str, data_source: str):
+        self.oracles[oracle_id].submit_data_source(data_source)
 
     def get_oracle(self, oracle_id: str):
         """
