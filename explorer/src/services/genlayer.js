60fed9841e3db136e01bdb56a374a79f296e225a
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-10-14 14:23:45 -0300

    add explorer skeleton
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

---
+++ explorer/src/services/genlayer.js
@@ -0,0 +1,15 @@
+import { createClient, createAccount as createGenLayerAccount, generatePrivateKey } from "genlayer-js";
+import { simulator } from "genlayer-js/chains";
+
+const accountPrivateKey = localStorage.getItem("accountPrivateKey")
+  ? localStorage.getItem("accountPrivateKey")
+  : null;
+export const account = accountPrivateKey ? createGenLayerAccount(accountPrivateKey) : null;
+
+export const createAccount = () => {
+  const newAccountPrivateKey = generatePrivateKey();
+  localStorage.setItem("accountPrivateKey", newAccountPrivateKey);
+  return createGenLayerAccount(newAccountPrivateKey);
+};
+
+export const client = createClient({ chain: simulator, account });
